967b86b1a6c582d352c543ee1661cba2059d1f88
NickTrinh <nhattvm1112@gmail.com>
2024-09-15 18:34:59 -0400

    First commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send("Sign up route called");
+};
+
+export const login = async (req, res) => {
+    res.send("Log in route called");
+};
+
+export const logout = async (req, res) => {
+    res.send("Log out route called");
+};
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// Routes
+import authRoutes from './routes/auth.route.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// Authentication
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log('Server running on https://localhost:' + PORT);
+});
