ae2caa82d86d6e9e9296c68e1d3fb978e74941db
NickTrinh <nhattvm1112@gmail.com>
2024-09-17 00:27:02 -0400

    minor changes to db.js

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
     try {
         const con = await mongoose.connect(process.env.MONGO_URI)
-        console.log('MongoDB connected: $(conn.connection.host)')
+        console.log(`MongoDB connected: ${con.connection.host}`)
     } catch (error) {
         console.log("Error connecting to MongoDB", error.Message)
         process.exit(1)
