711c31198acbb329610e8122e71f9df8a62818b3
Haothai1 <hao2003thai@gmail.com>
2024-09-24 22:21:44 -0700

    create & delete product

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getAllProducts } from '../controllers/product.controller.js';
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts } from '../controllers/product.controller.js';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-// protectRoute check accessToken is normal -> check if admin -> getAllProducts
-router.get('/', protectRoute, adminRoute, getAllProducts);
+router.get('/', protectRoute, adminRoute, getAllProducts); // protectRoute check accessToken is normal -> check if admin -> getAllProducts
+router.get("/featured", getFeaturedProducts);              // get featured products
+router.post("/", protectRoute, adminRoute, createProduct); // post product
+router.delete("/:id", protectRoute, adminRoute, deleteProduct); // delete product
 
 export default router;
