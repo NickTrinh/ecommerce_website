532c9c53b6322e4889d53d5609238d5a97023585
NickTrinh <nhattvm1112@gmail.com>
2024-10-08 16:35:26 -0400

    added login logout and check auth functions

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -1,29 +1,63 @@
 // checks user state, Auth, signin, logout
 
-import {create} from "zustand";
-import axios from "../lib/axios";
-import {toast} from "react-hot-toast";
+import { create } from 'zustand';
+import axios from '../lib/axios';
+import { toast } from 'react-hot-toast';
 
-export const useUserStore = create ((set,get) => ({
-   user: null,
-   loading:false,
-   checkingAuth: true,
+export const useUserStore = create((set, get) => ({
+	user: null,
+	loading: false,
+	checkingAuth: true,
 
-   signup: async ({name,email, password, confirmPassword}) => {
-      set({loading: true });
+	signup: async ({ name, email, password, confirmPassword }) => {
+		set({ loading: true });
 
-      if(password !== confirmPassword) {
-         set({ loading: false });
-         return toast.error("Password do not match")
-      }
+		if (password !== confirmPassword) {
+			set({ loading: false });
+			return toast.error('Password do not match');
+		}
 
-      try {
-         const res = await axios.post("/auth/signup", {name, email, password});
-         set({user: res.data.user, loading: false});
+		try {
+			const res = await axios.post('/auth/signup', { name, email, password });
+			set({ user: res.data.user, loading: false });
+		} catch (error) {
+			set({ loading: false });
+			toast.error(error.response.data.message || 'An error occurred');
+		}
+	},
 
-      } catch (error) {
-         set({ loading: false });
-         toast.error(error.response.data.message || "An error occurred");
-      }
-   },
+	login: async (email, password) => {
+		set({ loading: true });
+
+		try {
+			const res = await axios.post('/auth/login', { email, password });
+			set({ user: res.data, loading: false });
+		} catch (error) {
+			set({ loading: false });
+			toast.error(error.response.data.message || 'An error occurred');
+		}
+	},
+
+	logout: async () => {
+		try {
+			await axios.post('/auth/logout');
+			set({ user: null });
+		} catch (error) {
+			toast.error(
+				error.response?.data?.message || 'An error occurred during logout'
+			);
+		}
+	},
+
+	checkAuth: async () => {
+		set({ checkingAuth: true });
+		try {
+			const response = await axios.get('/auth/profile');
+			set({ user: response.data, checkingAuth: false });
+		} catch (error) {
+			set({ checkingAuth: false, user: null });
+		}
+	},
 }));
+
+// TODO: Implement the axios interceptors for refreshing access token
