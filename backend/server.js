4dddcf28e66c2bb8267ad0d198745f199b474063
NickTrinh <nhattvm1112@gmail.com>
2024-11-10 17:55:41 -0500

    setup for production

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 // Routes
 import authRoutes from './routes/auth.route.js';
@@ -17,6 +18,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: '10mb' })); // Allows parsing the body of the request
 app.use(cookieParser()); // Allows parsing cookies
 
@@ -27,6 +30,14 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/coupons', couponRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log('Server running on https://localhost:' + PORT);
