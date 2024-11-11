1f12e9bdb718283fba2c5ff443c757c5cfb22308
Haothai1 <hao2003thai@gmail.com>
2024-11-10 17:29:48 -0800

    Resolve vite.config.js conflict, port conflicts in frontend and backend fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+  server:{
+    port: 5173,
+    strictPort: true,
+    host: true,
+    proxy:{
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
       },
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 });
